d37c0f01606f2654bdfd00a6618e9d0d0cef65f4
Philipp Koller <phil.kolla@gmail.com>
2021-04-30 11:29:35 +0200

    added column name for paymentmethod

--- 6_query db.sql
+++ 6_query db.sql
@@ -1,5 +1,5 @@
 USE T4
-SELECT BillID, CONCAT( c.FirstName,' ',c.LastName) as Customer , PayDay, Amount, pm.name
+SELECT BillID, CONCAT( c.FirstName,' ',c.LastName) as Customer , PayDay, Amount, pm.name as PaymentMethod
 FROM BILLS p 
 JOIN PAYMENTMETHODS pm ON pm.PaymentMethodId = p.PaymentMethod
 JOIN CUSTOMER c ON c.CustomerId = p.CustomerID
